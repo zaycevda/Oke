e76a1b7470da2eb30f0333f0af93ed72f9d572e3
zaycevda <iusedtolikeu@gmail.com>
2023-06-07 18:39:20 +0300

    created NewsAdapter, get news from json

--- app/src/main/java/com/example/sportnews/MainActivity.kt
+++ app/src/main/java/com/example/sportnews/MainActivity.kt
@@ -2,10 +2,70 @@ package com.example.sportnews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.sportnews.databinding.ActivityMainBinding
+import org.json.JSONException
+import org.json.JSONObject
+import java.nio.charset.Charset
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding by viewBinding(ActivityMainBinding::bind, R.id.container)
+
+    private var adapter: NewsAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initAdapter()
+
+        jsonObjectsToAdapter()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        adapter = null
+    }
+
+    private fun initAdapter() {
+        adapter = NewsAdapter()
+        binding.rvNews.adapter = adapter
+    }
+
+    private fun jsonObjectsToAdapter() {
+        try {
+            jsonDataFromAsset()?.let {
+                val jsonObject = JSONObject(it)
+                val jsonArray = jsonObject.getJSONArray(NAME)
+                for (i in 0 until jsonArray.length()) {
+                    val newData = jsonArray.getJSONObject(i)
+                    adapter?.titles?.add(newData.getString("title"))
+                    adapter?.descriptions?.add(newData.getString("description"))
+                    adapter?.images?.add(newData.getString("urlToImage"))
+                    adapter?.links?.add(newData.getString("url"))
+                }
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun jsonDataFromAsset() =
+        try {
+            val inputStream = assets.open(FILENAME)
+            val sizeOfFile = inputStream.available()
+            val bufferData = ByteArray(sizeOfFile)
+            inputStream.read(bufferData)
+            inputStream.close()
+            String(bufferData, Charset.forName(FORMAT))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+
+    private companion object {
+        private const val FILENAME = "news.json"
+        private const val NAME = "news"
+        private const val FORMAT = "UTF-8"
     }
 }
---
+++ app/src/main/java/com/example/sportnews/NewsAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.sportnews
+
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.bumptech.glide.Glide
+import com.example.sportnews.databinding.ItemNewBinding
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
+
+    val titles = mutableListOf<String>()
+    val descriptions = mutableListOf<String>()
+    val images = mutableListOf<String>()
+    val links = mutableListOf<String>()
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val binding by viewBinding(ItemNewBinding::bind)
+
+        fun bind(position: Int) {
+            binding.tvTitle.text = titles[position]
+            binding.tvDescription.text = descriptions[position]
+            Glide.with(binding.root)
+                .load(images[position])
+                .into(binding.ivNew)
+
+            itemView.setOnClickListener {
+                openLink(links[position])
+            }
+        }
+
+        private fun openLink(link: String) {
+            val uri = Uri.parse(link)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            val context = itemView.context
+            context.startActivity(intent)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_new, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = titles.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+}
